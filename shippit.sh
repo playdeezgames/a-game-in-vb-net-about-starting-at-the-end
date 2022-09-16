5f0e5c17e1cb4d2de35963aaed1587a7a79aecce
TheGrumpyGameDev <ernestpazera@gmail.com>
2022-09-15 19:41:15 -0500

    shipped it!

--- shippit.sh
+++ shippit.sh
@@ -1,5 +1,5 @@
-dotnet publish ./src/AGameInVbNetAboutStartingAtTheEnd/AGameInVbNetAboutStartingAtTheEnd.vbproj -o ./pub-linux -c Release --sc -r linux-x64
-dotnet publish ./src/AGameInVbNetAboutStartingAtTheEnd/AGameInVbNetAboutStartingAtTheEnd.vbproj -o ./pub-windows -c Release --sc -r win-x64
+dotnet publish ./AGameInVbNetAboutStartingAtTheEnd/AGameInVbNetAboutStartingAtTheEnd.vbproj -o ./pub-linux -c Release --sc -r linux-x64
+dotnet publish ./AGameInVbNetAboutStartingAtTheEnd/AGameInVbNetAboutStartingAtTheEnd.vbproj -o ./pub-windows -c Release --sc -r win-x64
 butler push pub-windows thegrumpygamedev/a-game-in-vbnet-about-starting-at-the-end:windows
 butler push pub-linux thegrumpygamedev/a-game-in-vbnet-about-starting-at-the-end:linux
 git add -A
