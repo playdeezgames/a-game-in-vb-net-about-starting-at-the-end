9e1b58e9c31270f84020fc895a325d2693fbffbb
TheGrumpyGameDev <ernestpazera@gmail.com>
2022-09-16 12:53:18 -0500

    locationdata

--- AGIVNASATE.Data.Tests/CharacterDataTests.vb
+++ AGIVNASATE.Data.Tests/CharacterDataTests.vb
@@ -1,30 +1,35 @@
 ﻿Public Class CharacterDataTests
+    Private Sub WithCharacterData(stuffToDo As Action(Of Mock(Of IStore), Long, ICharacterData))
+        Const id = 1L
+        Dim store As New Mock(Of IStore)
+        Dim subject As ICharacterData = New CharacterData(store.Object)
+        stuffToDo(store, id, subject)
+        store.VerifyNoOtherCalls()
+    End Sub
     <Fact>
     Sub ShouldAttemptReadingALocationFromTheStore()
-        Const characterId = 1L
-        Dim store As New Mock(Of IStore)
-        Dim characterData As ICharacterData = New CharacterData(store.Object)
-        characterData.ReadLocation(characterId).ShouldBeNull
-        store.Verify(
+        WithCharacterData(
+            Sub(store, id, subject)
+                subject.ReadLocation(id).ShouldBeNull
+                store.Verify(
             Function(x) x.ReadColumnValue(Of Long, Long)(
                 It.IsAny(Of Action),
                 Tables.Characters,
                 Columns.LocationIdColumn,
-                (Columns.CharacterIdColumn, characterId)))
-        store.VerifyNoOtherCalls()
+                (Columns.CharacterIdColumn, id)))
+            End Sub)
     End Sub
     <Fact>
     Sub ShouldAttemptReadingANameFromTheStore()
-        Const characterId = 1L
-        Dim store As New Mock(Of IStore)
-        Dim characterData As ICharacterData = New CharacterData(store.Object)
-        characterData.ReadName(characterId).ShouldBeNull
-        store.Verify(
-            Function(x) x.ReadColumnString(Of Long)(
-                It.IsAny(Of Action),
-                Tables.Characters,
-                Columns.CharacterNameColumn,
-                (Columns.CharacterIdColumn, characterId)))
-        store.VerifyNoOtherCalls()
+        WithCharacterData(
+            Sub(store, id, subject)
+                subject.ReadName(id).ShouldBeNull
+                store.Verify(
+                    Function(x) x.ReadColumnString(Of Long)(
+                        It.IsAny(Of Action),
+                        Tables.Characters,
+                        Columns.CharacterNameColumn,
+                        (Columns.CharacterIdColumn, id)))
+            End Sub)
     End Sub
 End Class
---
+++ AGIVNASATE.Data.Tests/LocationDataTests.vb
@@ -0,0 +1,22 @@
+﻿Public Class LocationDataTests
+    Private Sub WithLocationData(stuffToDo As Action(Of Mock(Of IStore), Long, ILocationData))
+        Const id = 1L
+        Dim store As New Mock(Of IStore)
+        Dim subject As ILocationData = New LocationData(store.Object)
+        stuffToDo(store, id, subject)
+        store.VerifyNoOtherCalls()
+    End Sub
+    <Fact>
+    Sub ShouldAttemptReadingTheNameOfALocationFromTheStore()
+        WithLocationData(
+            Sub(store, id, subject)
+                subject.ReadName(id).ShouldBeNull
+                store.Verify(
+                    Function(x) x.ReadColumnString(
+                        It.IsAny(Of Action),
+                        Tables.Locations,
+                        Columns.LocationNameColumn,
+                        (Columns.LocationIdColumn, id)))
+            End Sub)
+    End Sub
+End Class
