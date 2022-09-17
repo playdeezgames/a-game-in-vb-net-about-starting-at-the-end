ba1d0c1e727d91a20d215941caf63d01638ce2ea
TheGrumpyGameDev <ernestpazera@gmail.com>
2022-09-17 04:45:42 -0500

    ShouldHoldAnIdNumber

---
+++ AGIVNASATE.Game.Tests/RouteTests.vb
@@ -0,0 +1,16 @@
+﻿Public Class RouteTests
+    Private Sub WithSubject(stuffToDo As Action(Of Mock(Of IWorldData), Long, IRoute))
+        Const id = 1L
+        Dim worldData As New Mock(Of IWorldData)
+        Dim subject As IRoute = New Route(worldData.Object, id)
+        stuffToDo(worldData, id, subject)
+        worldData.VerifyNoOtherCalls()
+    End Sub
+    <Fact>
+    Sub ShouldHoldAnIdNumber()
+        WithSubject(
+            Sub(worldData, id, subject)
+                subject.Id.ShouldBe(id)
+            End Sub)
+    End Sub
+End Class
