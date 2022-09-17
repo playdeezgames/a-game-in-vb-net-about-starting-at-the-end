5d2efd44516cfa243bd93e26aaf7df0da2af609d
TheGrumpyGameDev <ernestpazera@gmail.com>
2022-09-17 04:28:42 -0500

    refactored character tests

--- AGIVNASATE.Game.Tests/CharacterTests.vb
+++ AGIVNASATE.Game.Tests/CharacterTests.vb
@@ -1,30 +1,34 @@
 ﻿Public Class CharacterTests
-    <Fact>
-    Sub ShouldHoldAnIdNumber()
+    Private Sub WithSubject(stuffToDo As Action(Of Mock(Of IWorldData), Long, ICharacter))
         Const id = 1L
         Dim worldData As New Mock(Of IWorldData)
         Dim subject As ICharacter = New Character(worldData.Object, id)
-        subject.Id.ShouldBe(id)
+        stuffToDo(worldData, id, subject)
         worldData.VerifyNoOtherCalls()
+    End Sub
+    <Fact>
+    Sub ShouldHoldAnIdNumber()
+        WithSubject(
+            Sub(worldData, id, subject)
+                subject.Id.ShouldBe(id)
+            End Sub)
     End Sub
     <Fact>
     Sub ShouldAttempToReadLocationForACharacterFromTheData()
-        Const id = 1L
-        Dim worldData As New Mock(Of IWorldData)
-        worldData.SetupGet(Function(x) x.Character).Returns((New Mock(Of ICharacterData)).Object)
-        Dim subject As ICharacter = New Character(worldData.Object, id)
-        subject.Location.ShouldBeNull
-        worldData.Verify(Function(x) x.Character.ReadLocation(id))
-        worldData.VerifyNoOtherCalls()
+        WithSubject(
+            Sub(worldData, id, subject)
+                worldData.SetupGet(Function(x) x.Character).Returns((New Mock(Of ICharacterData)).Object)
+                subject.Location.ShouldBeNull
+                worldData.Verify(Function(x) x.Character.ReadLocation(id))
+            End Sub)
     End Sub
     <Fact>
     Sub ShouldAttemptToReadNameForACharacterFromTheData()
-        Const id = 1L
-        Dim worldData As New Mock(Of IWorldData)
-        worldData.SetupGet(Function(x) x.Character).Returns((New Mock(Of ICharacterData)).Object)
-        Dim subject As ICharacter = New Character(worldData.Object, id)
-        subject.Name.ShouldBeNull
-        worldData.Verify(Function(x) x.Character.ReadName(id))
-        worldData.VerifyNoOtherCalls()
+        WithSubject(
+            Sub(worldData, id, subject)
+                worldData.SetupGet(Function(x) x.Character).Returns((New Mock(Of ICharacterData)).Object)
+                subject.Name.ShouldBeNull
+                worldData.Verify(Function(x) x.Character.ReadName(id))
+            End Sub)
     End Sub
 End Class
