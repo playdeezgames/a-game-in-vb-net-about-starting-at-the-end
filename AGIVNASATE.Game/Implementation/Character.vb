6a2ae21a4b0ff704e52ef02411ac7038f7658ffb
TheGrumpyGameDev <ernestpazera@gmail.com>
2022-09-18 04:40:47 -0500

    remove blank lines

--- AGIVNASATE.Game/Implementation/Character.vb
+++ AGIVNASATE.Game/Implementation/Character.vb
@@ -1,23 +1,19 @@
 ﻿Public Class Character
     Inherits BaseThingie
     Implements ICharacter
-
     Public Sub New(worlddata As IWorldData, id As Long)
         MyBase.New(worlddata, id)
     End Sub
-
     Public ReadOnly Property Name As String Implements ICharacter.Name
         Get
             Return WorldData.Character.ReadName(Id)
         End Get
     End Property
-
     Public ReadOnly Property Location As ILocation Implements ICharacter.Location
         Get
             Return AGIVNASATE.Game.Location.FromId(WorldData, WorldData.Character.ReadLocation(Id))
         End Get
     End Property
-
     Public ReadOnly Property Satiety As (Long, Long) Implements ICharacter.Satiety
         Get
             Dim maxSatiety = MaximumSatiety
@@ -55,24 +51,20 @@
         End Get
     End Property
     Const AttackStatisticTypeId = 5L
-
     Public ReadOnly Property Attack As Long Implements ICharacter.Attack
         Get
             Return If(WorldData.CharacterStatistic.Read(Id, AttackStatisticTypeId), 0)
         End Get
     End Property
-
     Const DefendStatisticTypeId = 6L
     Public ReadOnly Property Defend As Long Implements ICharacter.Defend
         Get
             Return If(WorldData.CharacterStatistic.Read(Id, DefendStatisticTypeId), 0)
         End Get
     End Property
-
     Public Sub Move(route As IRoute) Implements ICharacter.Move
         WorldData.Character.WriteLocation(Id, route.ToLocation.Id)
     End Sub
-
     Public Shared Function FromId(worldData As IWorldData, id As Long?) As ICharacter
         Return If(id.HasValue, New Character(worldData, id.Value), Nothing)
     End Function
