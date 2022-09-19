202fdc26efd9a68030a2d99a2e4b81c4f1dc6ff1
TheGrumpyGameDev <ernestpazera@gmail.com>
2022-09-19 04:46:42 -0500

    character combat moved out of character part i

---
+++ AGIVNASATE.Game.Tests/CharacterCombatTests.vb
@@ -0,0 +1,9 @@
+﻿Public Class CharacterCombatTests
+    Private Sub WithSubject(stuffToDo As Action(Of Mock(Of IWorldData), Long, ICharacterCombat))
+        Const id = 1L
+        Dim worldData As New Mock(Of IWorldData)
+        Dim subject As ICharacterCombat = New CharacterCombat(worldData.Object, id)
+        stuffToDo(worldData, id, subject)
+        worldData.VerifyNoOtherCalls()
+    End Sub
+End Class
--- AGIVNASATE.Game/Implementation/CharacterCombat.vb
+++ AGIVNASATE.Game/Implementation/CharacterCombat.vb
@@ -6,7 +6,7 @@
         MyBase.New(worlddata, id)
     End Sub
 
-    Friend Shared Function FromId(worldData As IWorldData, id As Long) As ICharacterCombat
+    Public Shared Function FromId(worldData As IWorldData, id As Long) As ICharacterCombat
         Return New CharacterCombat(worldData, id)
     End Function
 End Class
