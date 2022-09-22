08975c7bef6e71f4c6421be32e99f0df532e143f
TheGrumpyGameDev <ernestpazera@gmail.com>
2022-09-22 04:34:49 -0500

    ShouldCreateAnItemType

---
+++ AGIVNASATE.Game.Tests/ItemTests.vb
@@ -0,0 +1,15 @@
+﻿Public Class ItemTests
+    Inherits BaseGameTests(Of IItem)
+
+    Public Sub New()
+        MyBase.New(AddressOf Item.FromId)
+    End Sub
+
+    <Fact>
+    Sub ShouldCreateAnItemType()
+        WithSubject(
+            Sub(worldData, id, subject)
+                subject.ShouldNotBeNull
+            End Sub)
+    End Sub
+End Class
--- AGIVNASATE.Game/Item.vb
+++ AGIVNASATE.Game/Item.vb
@@ -1,6 +1,12 @@
 ﻿Public Class Item
+    Inherits BaseThingie
     Implements IItem
+
+    Public Sub New(worlddata As IWorldData, id As Long)
+        MyBase.New(worlddata, id)
+    End Sub
+
     Public Shared Function FromId(worldData As IWorldData, id As Long?) As IItem
-        Throw New NotImplementedException
+        Return If(id.HasValue, New Item(worldData, id.Value), Nothing)
     End Function
 End Class
