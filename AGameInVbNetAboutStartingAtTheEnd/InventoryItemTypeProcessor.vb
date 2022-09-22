bf65f12ed96655398ff2b81967ae541f0a7955e5
TheGrumpyGameDev <ernestpazera@gmail.com>
2022-09-22 04:41:39 -0500

    inventory item type screen stub

---
+++ AGameInVbNetAboutStartingAtTheEnd/InventoryItemTypeProcessor.vb
@@ -0,0 +1,15 @@
+﻿Module InventoryItemTypeProcessor
+    Friend Sub Run(character As ICharacter, itemType As IItemType)
+        Dim done = False
+        While Not done
+            AnsiConsole.Clear()
+            Dim prompt As New SelectionPrompt(Of String) With {.Title = $"{character.Name}'s {itemType.Name}(s)"}
+            prompt.AddChoice(NeverMindText)
+            Dim answer = AnsiConsole.Prompt(prompt)
+            Select Case answer
+                Case NeverMindText
+                    done = True
+            End Select
+        End While
+    End Sub
+End Module
--- AGameInVbNetAboutStartingAtTheEnd/InventoryProcessor.vb
+++ AGameInVbNetAboutStartingAtTheEnd/InventoryProcessor.vb
@@ -9,10 +9,14 @@
                 ToDictionary(
                     Function(x) $"{x.Key.Name}(x{x.Value.Count})",
                     Function(x) x.Key)
+            prompt.AddChoices(table.Keys)
             Dim answer = AnsiConsole.Prompt(prompt)
             Select Case answer
                 Case NeverMindText
                     done = True
+                Case Else
+                    Dim itemType = table(answer)
+                    InventoryItemTypeProcessor.Run(character, itemType)
             End Select
         End While
     End Sub
